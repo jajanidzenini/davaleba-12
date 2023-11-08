e828a489ad09648eabfe132ee50d5961af6693a4
nini jajanidze <jajanidzenini23@gmail.com>
2023-11-08 20:36:40 +0400

    Initial Commit

---
+++ nini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nini
+//
+//  Created by niniku on 08.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
